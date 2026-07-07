3f14c7fedd93e972883594b9c29dbd158627072b
Coded5 <phoenixanimation24@gmail.com>
2026-07-07 23:11:01 +0700

    hypr: add hyprpc

---
+++ hyprpc/.config/hypr/modules/monitors.lua
@@ -0,0 +1,13 @@
+hl.monitor({
+	output = "DP-2",
+	mode = "2560x1440@200",
+	position = "0x0",
+	scale = "1",
+})
+
+hl.monitor({
+	output = "DP-1",
+	mode = "2560x1440@200",
+	position = "-2560x0",
+	scale = "1",
+})
---
+++ hyprpc/.config/hypr/modules/workspaces.lua
@@ -0,0 +1,17 @@
+for w = 1, 4 do
+	hl.workspace_rule({
+		workspace = tostring(w),
+		monitor = "DP-2",
+		default = true,
+		persistent = true,
+	})
+end
+
+for w = 5, 8 do
+	hl.workspace_rule({
+		workspace = tostring(w),
+		monitor = "DP-1",
+		default = true,
+		persistent = true,
+	})
+end
