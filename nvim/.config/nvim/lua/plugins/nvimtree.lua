75f4282774921965b8a80d2942ce0be6336a5b32
Coded5 <phoenixanimation24@gmail.com>
2026-07-21 20:27:24 +0700

    nvim: open zathura when open pdf

--- nvim/.config/nvim/lua/plugins/nvimtree.lua
+++ nvim/.config/nvim/lua/plugins/nvimtree.lua
@@ -10,12 +10,46 @@ return {
 			vim.g.loaded_netrw = 1
 			vim.g.loaded_netrwPlugin = 1
 
-			require("nvim-tree").setup({
+			local nvim_tree = require("nvim-tree")
+
+			nvim_tree.setup({
 				view = {
 					width = 40,
 					side = "right",
 					relativenumber = true,
 				},
+				on_attach = function(bufnr)
+					local api = require("nvim-tree.api")
+
+					-- Keep all default nvim-tree mappings.
+					-- api.config.mappings.default_on_attach(bufnr)
+					api.map.on_attach.default(bufnr)
+
+					local function open_file()
+						local node = api.tree.get_node_under_cursor()
+
+						if not node or node.type == "directory" then
+							api.node.open.edit()
+							return
+						end
+
+						local path = node.absolute_path
+						local extension = vim.fn.fnamemodify(path, ":e"):lower()
+
+						if extension == "pdf" then
+							vim.system({ "zathura", path }, { detach = true })
+						else
+							api.node.open.edit()
+						end
+					end
+
+					vim.keymap.set("n", "<CR>", open_file, {
+						buffer = bufnr,
+						noremap = true,
+						silent = true,
+						desc = "Open file",
+					})
+				end,
 			})
 		end,
 	},
