23a3aacc25ccff2ec9eb9a61486c8d7a496667a9
Coded5 <phoenixanimation24@gmail.com>
2026-07-23 20:35:00 +0700

    nvim: move tailwindcss to webdev group

--- nvim/.config/nvim/lua/plugins/webdev.lua
+++ nvim/.config/nvim/lua/plugins/webdev.lua
@@ -5,4 +5,15 @@ return {
 			vim.keymap.set({ "n", "v" }, "<leader>xe", require("nvim-emmet").wrap_with_abbreviation)
 		end,
 	},
+    {
+        "luckasRanarison/tailwind-tools.nvim",
+        enabled = false,
+        name = "tailwind-tools",
+        build = ":UpdateRemotePlugins",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-telescope/telescope.nvim", -- optional
+        },
+        opts = {}                            -- your configuration
+    }
 }
