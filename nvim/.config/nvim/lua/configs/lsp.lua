1f2073b12086dc817bc0a1b39ae20cfba906326f
Coded5 <phoenixanimation24@gmail.com>
2026-07-23 20:31:25 +0700

    nvim: change eslint lsp config

--- nvim/.config/nvim/lua/configs/lsp.lua
+++ nvim/.config/nvim/lua/configs/lsp.lua
@@ -1,14 +1,13 @@
 vim.lsp.config("svelte", require("configs.lsp.svelte"))
+vim.lsp.config("eslint", {
+	settings = {
+		run = "onSave",
+	},
+})
 
 vim.lsp.enable("prolog")
 
 vim.diagnostic.config({
 	virtual_text = true,
 	virtual_lines = false,
-})
-
-vim.filetype.add({
-	extension = {
-		pl = "prolog",
-	},
 })
