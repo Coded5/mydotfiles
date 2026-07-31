b820da21528eded766a32408844b955b45d2937d
Coded5 <phoenixanimation24@gmail.com>
2026-07-31 20:43:59 +0700

    hypr: swap monitors

--- hyprpc/.config/hypr/modules/monitors.lua
+++ hyprpc/.config/hypr/modules/monitors.lua
@@ -1,12 +1,12 @@
 hl.monitor({
-	output = "DP-2",
+	output = "DP-1",
 	mode = "2560x1440@200",
 	position = "0x0",
 	scale = "1",
 })
 
 hl.monitor({
-	output = "DP-1",
+	output = "DP-2",
 	mode = "2560x1440@200",
 	position = "-2560x0",
 	scale = "1",
--- hyprpc/.config/hypr/modules/workspaces.lua
+++ hyprpc/.config/hypr/modules/workspaces.lua
@@ -1,7 +1,7 @@
 for w = 1, 4 do
 	hl.workspace_rule({
 		workspace = tostring(w),
-		monitor = "DP-2",
+		monitor = "DP-1",
 		default = true,
 		persistent = true,
 	})
@@ -10,7 +10,7 @@ end
 for w = 5, 8 do
 	hl.workspace_rule({
 		workspace = tostring(w),
-		monitor = "DP-1",
+		monitor = "DP-2",
 		default = true,
 		persistent = true,
 	})
