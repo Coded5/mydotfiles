c777be66ec6021dc238b103d4b0a6aa425d60799
Coded5 <phoenixanimation24@gmail.com>
2026-08-02 23:20:50 +0700

    hypr: refactor environment variables

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -23,13 +23,8 @@ end)
 ---- ENVIRONMENT VARIABLES ----
 -------------------------------
 
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+require("modules.environments")
 
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
-hl.env("HYPRSHOT_DIR", "/home/kami/Pictures/Hyprshot")
-hl.env("ELECTRON_OZONE_PLATFORM_HINT", "wayland")
-hl.env("LIBVA_DRIVER_NAME", "nvidia")
 -----------------------
 ----- PERMISSIONS -----
 -----------------------
---
+++ hyprpc/.config/hypr/modules/environments.lua
@@ -0,0 +1,6 @@
+-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("HYPRSHOT_DIR", "/home/kami/Pictures/Hyprshot")
+hl.env("ELECTRON_OZONE_PLATFORM_HINT", "wayland")
+hl.env("LIBVA_DRIVER_NAME", "nvidia")
