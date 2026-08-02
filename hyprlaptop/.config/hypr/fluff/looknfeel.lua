f1098d297e947a8fbdcff3ab19b32aa4a78cbaac
Coded5 <phoenixanimation24@gmail.com>
2026-08-02 14:41:00 +0700

    hypr: refactor look and feel

---
+++ hyprlaptop/.config/hypr/fluff/looknfeel.lua
@@ -0,0 +1,73 @@
+hl.config({
+	general = {
+		gaps_in = 6,
+		gaps_out = 10,
+
+		border_size = 1,
+
+		col = {
+			active_border = "rgba(c2e5f2ff)",
+			inactive_border = "rgba(595959aa)",
+		},
+
+		-- Set to true to enable resizing windows by clicking and dragging on borders and gaps
+		resize_on_border = false,
+
+		-- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
+		allow_tearing = false,
+
+		layout = "dwindle",
+	},
+
+	decoration = {
+		rounding = 4,
+		rounding_power = 4,
+
+		-- Change transparency of focused and unfocused windows
+		active_opacity = 1.0,
+		inactive_opacity = 0.95,
+
+		shadow = {
+			enabled = true,
+			range = 4,
+			render_power = 3,
+			color = 0xee1a1a1a,
+		},
+
+		blur = {
+			enabled = true,
+			size = 3,
+			passes = 1,
+			vibrancy = 0.1696,
+		},
+	},
+
+	animations = {
+		enabled = true,
+	},
+
+	cursor = {
+		inactive_timeout = 3,
+	},
+})
+
+-- See https://wiki.hypr.land/Configuring/Layouts/Dwindle-Layout/ for more
+hl.config({
+	dwindle = {
+		preserve_split = true, -- You probably want this
+	},
+})
+
+-- See https://wiki.hypr.land/Configuring/Layouts/Master-Layout/ for more
+hl.config({
+	master = {
+		new_status = "master",
+	},
+})
+
+-- See https://wiki.hypr.land/Configuring/Layouts/Scrolling-Layout/ for more
+hl.config({
+	scrolling = {
+		fullscreen_on_one_column = true,
+	},
+})
