715d013b6093659aa3f1f437995e30e20c9492b1
Coded5 <phoenixanimation24@gmail.com>
2026-08-18 20:48:57 +0700

    hypr: laptop workspace qol

--- hyprlaptop/.config/hypr/modules/workspaces.lua
+++ hyprlaptop/.config/hypr/modules/workspaces.lua
@@ -1,4 +1,4 @@
-for w = 1, 8 do
+for w = 1, 6 do
 	hl.workspace_rule({
 		workspace = tostring(w),
 		monitor = "eDP-1",
@@ -6,3 +6,31 @@ for w = 1, 8 do
 		persistent = true,
 	})
 end
+
+-- Workspace 1: Terminal
+hl.window_rule({
+	name = "workspace-1-terminal",
+	match = { class = "kitty" },
+	workspace = "1 silent",
+})
+
+-- Workspace 2: Zen Browser
+hl.window_rule({
+	name = "workspace-2-zen",
+	match = { class = "zen" },
+	workspace = "2 silent",
+})
+
+-- Workspace 3: Obsidian
+hl.window_rule({
+	name = "workspace-3-obsidian",
+	match = { class = "md.obsidian.Obsidian" },
+	workspace = "3 silent",
+})
+
+-- Workspace 4: Discord
+hl.window_rule({
+	name = "workspace-3-discord",
+	match = { class = "discord" },
+	workspace = "4 silent",
+})
