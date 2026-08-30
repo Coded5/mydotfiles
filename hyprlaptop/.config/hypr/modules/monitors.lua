b3bc9e75f1c2054c36dce9974cc47423a93d6fd9
Coded5 <phoenixanimation24@gmail.com>
2026-08-30 20:14:35 +0700

    optimize of laptop battery

--- hyprlaptop/.config/hypr/modules/monitors.lua
+++ hyprlaptop/.config/hypr/modules/monitors.lua
@@ -6,7 +6,12 @@ hl.config({
 
 hl.monitor({
 	output = "eDP-1",
-	mode = "2880x1800@90",
+	mode = "2880x1800@60",
 	position = "0x0",
 	scale = "1.2",
+})
+
+hl.device({
+    name = "pixa3835:00-093a:3835-touchpad",
+    sensitivity = 0.2
 })
--- hyprlaptop/.config/hypr/modules/workspaces.lua
+++ hyprlaptop/.config/hypr/modules/workspaces.lua
@@ -11,26 +11,26 @@ end
 hl.window_rule({
 	name = "workspace-1-terminal",
 	match = { class = "kitty" },
-	workspace = "1 silent",
+	workspace = "1",
 })
 
 -- Workspace 2: Zen Browser
 hl.window_rule({
 	name = "workspace-2-zen",
 	match = { class = "zen" },
-	workspace = "2 silent",
+	workspace = "2",
 })
 
 -- Workspace 3: Obsidian
 hl.window_rule({
 	name = "workspace-3-obsidian",
 	match = { class = "md.obsidian.Obsidian" },
-	workspace = "3 silent",
+	workspace = "3",
 })
 
 -- Workspace 4: Discord
 hl.window_rule({
 	name = "workspace-3-discord",
 	match = { class = "discord" },
-	workspace = "4 silent",
+	workspace = "4",
 })
