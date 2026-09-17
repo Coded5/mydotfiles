97d2f1534256795ad1743f68a6961dbf7e05cb71
Coded5 <phoenixanimation24@gmail.com>
2026-09-18 01:01:39 +0700

    nvim: enable moonfly colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -16,16 +16,15 @@ return {
 	{
 		"bluz71/vim-moonfly-colors",
 		name = "moonfly",
-		enabled = false,
 		lazy = false,
 		priority = 1000,
-		config = function()
-			require("moonfly").custom_colors({
-				grey11 = "#474747",
-			})
-			vim.g.moonflyTransparent = true
-			vim.g.moonflyVirtualTextColor = true
-			vim.cmd.colorscheme("moonfly")
+		_config = function()
+			-- require("moonfly").custom_colors({
+			-- 	grey11 = "#474747",
+			-- })
+			-- vim.g.moonflyTransparent = true
+			-- vim.g.moonflyVirtualTextColor = true
+			-- vim.cmd.colorscheme("moonfly")
 		end,
 	},
 	{
